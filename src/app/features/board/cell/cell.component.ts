e0898ecfdeacc684650c3ace4c77160ff49e3523
Pablo <pablo.esquivel@hotmail.com>
2024-02-25 15:25:39 -0300

    Basic modules and cell component

---
+++ src/app/features/board/cell/cell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cell',
+  standalone: true,
+  imports: [],
+  templateUrl: './cell.component.html',
+  styleUrl: './cell.component.scss',
+})
+export class CellComponent {}
